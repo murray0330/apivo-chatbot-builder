dfaa620f35e76a17ce0a118a7e27d9101573ff15
murray0330 <158100395+murray0330@users.noreply.github.com>
2026-02-26 13:50:29 -0500

    Fix primaryColor hex code in clients.ts

--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -69,7 +69,7 @@ const clients: Record<string, ClientConfig> = {
       "New Patient",
       "Existing Patient",
     ],
-  primaryColor: "#4B0082",
+  primaryColor: "##615FFF",
   description: "AI Booking Assistant",
   footer: "Powered by Apivo",
   launcherIcon: "message",
